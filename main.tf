b77aab88ce1eff2b26e168e266d0d1bebac1f30a
SkalingClouds <chris.skaling@gmail.com>
2019-12-28 21:04:13 -0800

    tf files

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+    version = "~>1.5"
+}
+
+terraform {
+    backend "azurerm" {}
+}
+provider "azurerm" {
+    version = "~>1.5"
+}
+
+terraform {
+    backend "azurerm" {}
+}
